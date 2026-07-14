ee19c907550b6241c905833084770614971a201e
artur-vol <volunetsartur@gmail.com>
2026-07-14 17:04:42 +0300

    Add Note model

---
+++ src/main/java/com/example/notes/model/Note.java
@@ -0,0 +1,53 @@
+package com.example.notes.model;
+
+import java.time.LocalDateTime;
+
+public class Note {
+
+    private int id;
+    private String title;
+    private String content;
+    private LocalDateTime createdAt;
+
+    public Note() {
+    }
+
+    public Note(int id, String title, String content, LocalDateTime createdAt) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.createdAt = createdAt;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+}
